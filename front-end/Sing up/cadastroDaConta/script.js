4db6b8efbad80b4d45acf89817169177697118a9
Gustavo <166347161+Gustavomw1@users.noreply.github.com>
2024-12-13 17:03:43 -0300

    front end sendo feito

---
+++ front-end/Sing up/cadastroDaConta/script.js
@@ -0,0 +1,32 @@
+const API_URL = "http://localhost:8000";
+
+// Função para cadastrar usuário
+async function registerUser() {
+  const usuario = document.getElementById("usuario").value;
+  const password = document.getElementById("password").value;
+
+  if (!usuario || !password) {
+    alert("Por favor, preencha todos os campos!");
+    return;
+  }
+  try {
+    const response = await fetch(`${API_URL}/cadastrar`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ usuario, password }),
+    });
+
+    const data = await response.json();
+    alert(data.mensagem);
+
+    // Redirecionar para index2.html após o cadastro
+    if (response.ok) {
+      window.location.href = "../../contaLogada/loja/index.html"
+    }
+  } catch (error) {
+    console.error("Erro ao cadastrar usuário:", error);
+    alert("Erro ao cadastrar usuário.");
+  }
+}
---
+++ front-end/Sing up/logarConta/script.js
@@ -0,0 +1,37 @@
+const API_URL = "http://localhost:8000";
+
+// Função para fazer login do usuário
+async function loginUser() {
+  const usuario = document.getElementById("usuario").value;
+  const password = document.getElementById("password").value;
+
+  if (!usuario || !password) {
+    alert("Por favor, preencha todos os campos!");
+    return;
+  }
+
+  try {
+    const response = await fetch(`${API_URL}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ usuario, password }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      alert("Login bem-sucedido!"); 
+    } else {
+      alert(data.erro || "Erro ao fazer login."); 
+    }
+
+    if (response.ok) {
+      window.location.href = "../../contaLogada/loja/index.html";
+    }
+  } catch (error) {
+    console.error("Erro ao fazer login:", error);
+    alert("Erro ao fazer login.");
+  }
+}
